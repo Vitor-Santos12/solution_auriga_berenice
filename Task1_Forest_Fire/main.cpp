80a5a8b72f79045009e58371dc6b178a0d64ab98
Vitor Santos <vitor.santos.ferreira21@gmail.com>
2024-08-19 21:41:11 -0300

    first commit

---
+++ Task1_Forest_Fire/main.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(){
+
+int airplaneBound = 50;
+int airplaneNumber = 100;
+int x, y = 0;
+
+int L = 1000; //0 < L <= 1000
+int N = 100; // 1 <= N <= 100
+
+int x0, y0, x1, y1 = 0 ;
+
+int input1 = 120;
+int input2 = 12;
+float myNumbers[] = {17.4,23,33.27,99.861};
+
+printf("OK!\t %d %d %d %d", x0, y0, x1, y1);
+
+return 0;
+
+}
+
