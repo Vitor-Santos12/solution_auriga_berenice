17ec029efe201b48e32fcd5bac585538225719b2
Vitor Santos <vitor.santos.ferreira21@gmail.com>
2024-08-19 22:09:27 -0300

    desktop code

--- Task1_Forest_Fire/main.cpp
+++ Task1_Forest_Fire/main.cpp
@@ -1,5 +1,8 @@
 #include <stdio.h>
 
+
+
+
 int main(){
 
 int airplaneBound = 50;
@@ -13,7 +16,9 @@ int x0, y0, x1, y1 = 0 ;
 
 int input1 = 120;
 int input2 = 12;
-float myNumbers[] = {17.4,23,33.27,99.861};
+
+
+
 
 printf("OK!\t %d %d %d %d", x0, y0, x1, y1);
 
